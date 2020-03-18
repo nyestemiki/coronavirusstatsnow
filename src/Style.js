099f71d09a04c6a3cec18d012816fa1256a6c5fb
Nyeste Miki <nyeste.miki@yahoo.com>
2020-03-18 16:19:48 +0200

    Initial commit

---
+++ src/Style.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+const Style = styled.div`
+    background: rgba(0, 0, 0, .75);
+    min-height: 100vh;
+    max-height: 100%;
+    position: relative;
+`;
+
+const Top = styled.div`
+    display: flex;
+    place-content: space-between;
+    backdrop-filter: blur(10px);
+    background: rgba(255, 255, 255, .5);
+    padding: 1rem 2rem;
+    position: fixed;
+    width: 100vw;
+    z-index: 100;
+
+    & > * {
+        background: rgba(255, 255, 255, .5);
+        padding: 5px;
+        border-radius: 5px;
+        transition: background-color .5s;
+        cursor: pointer;
+
+        &:hover {
+            background: rgba(255, 255, 255, .75);
+            box-shadow: 0 0 5px black;
+        }
+    }
+`;
+
+const Select = styled.select`
+    max-width: 50vw;  
+`;
+
+const Language = styled.div`
+    display: flex;
+    place-items: space-around;
+    place-content: center;
+    
+    img {
+        width: 2rem;
+        margin: 2px;
+        transition: transform .6s;
+
+        &:hover {
+            transform: scale(1.25);
+        }
+    }
+`;
+
+const Data = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    place-content: space-around;
+    place-items: center;
+    position: relative;
+    min-height: 100vh;
+    max-height: 100%;
+`;
+
+const DataContainer = styled.div`
+    padding: 5rem;
+    margin: 5rem;
+    border-radius: 10px;
+    background: rgba(255, 255, 255, .6);
+    display: flex;
+    flex-direction: column;
+    place-content: center;
+    place-items: center;
+    box-shadow: 0 0 10px black;
+`;
+
+const Number = styled.div`
+    font-size: 3rem;
+    font-weight: bold;
+`;
+
+const Title = styled.div`
+    font-size: 1.35rem;
+`;
+
+const Updated = styled.div`
+    position: fixed;
+    bottom: 0;
+    color: white;
+    display: flex;
+    place-items: center;
+    place-content: center;
+    background: rgba(255, 255, 255, .25);
+    padding: 5px;
+    width: 100vw;
+    backdrop-filter: blur(10px);
+    
+    & > * {
+        padding: 3px;
+    }
+`;
+
+export { Style, Top, Select, Language, Data, DataContainer, Number, Title, Updated };
---
+++ src/localization.js
@@ -0,0 +1,34 @@
+const localization = {
+    "World": {
+        en: "Around the World",
+        hu: "A világban",
+        ro: "În lume",
+        de: "In der Welt"
+    },
+    "Confirmed": {
+        en: "Confirmed",
+        hu: "Fertőzött",
+        ro: "Confirmați",
+        de: "Bestätigt"
+    },
+    "Recovered": {
+        en: "Recovered",
+        hu: "Gyógyult",
+        ro: "Recuperați",
+        de: "Geheilt"
+    }, 
+    "Deaths": {
+        en: "Deaths",
+        hu: "Halott",
+        ro: "Decese",
+        de: "Todesfälle"
+    },
+    "Updated": {
+        en: "Updated",
+        hu: "Frissítve",
+        ro: "Actualizat",
+        de: "Aktualisiert"
+    }
+};
+
+export default localization;
