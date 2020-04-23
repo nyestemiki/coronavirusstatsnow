2a1f954e40e6c9b09d31bb5d8b2ac7f009d47308
Nyeste Miki <nyeste.miki@yahoo.com>
2020-04-23 21:07:11 +0300

    Countup

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import localization from '../Utils/localization';
-import { formatNumber } from '../Utils/format';
 import { DataContainer, Number, Title } from '../Style';
+import CountUp from 'react-countup';
 
 // Tile
 export default ({ title, number }) => (
     <DataContainer>
-        <Number>{formatNumber(number)}</Number>
+        <Number>
+            <CountUp start={0} end={number ?? 0} delay={0} duration={1} separator=",">
+            {({ countUpRef }) => <div><span ref={countUpRef}/></div>}
+            </CountUp>
+        </Number>
         <Title>{localization(title)}</Title>
     </DataContainer>
 )
