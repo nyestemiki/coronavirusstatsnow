7cb72830c10c5f586689dad93eeb01a22b53b9ec
Nyeste Miki <nyeste.miki@yahoo.com>
2020-04-25 16:48:28 +0300

    Failed hook test

--- __test__/localization.test.js
+++
@@ -1,7 +0,0 @@
-import localization from '../src/Utils/localization';
-
-describe('localization Function', () => {
-    it.skip('works as expected', () => {
-        expect(1).toEqual(1);
-    });
-});
