d70fa374c8d2eb73524a6d22f808ebdc4692c46f
Nyeste Miki <nyeste.miki@yahoo.com>
2020-04-25 16:46:10 +0300

    formatDate tested

--- __test__/formatDate.test.js
+++ __test__/formatDate.test.js
@@ -1,7 +1,18 @@
 import { formatDate } from '../src/Utils/format';
 
 describe('formatDate Function', () => {
-    it('works as expected', () => {
-        expect(1).toEqual(1);
+    it('does not put 0 before single-digit hour', () => {
+        expect(formatDate(new Date('1995-12-17T03:24:00')))
+            .toEqual('12/17/1995 3:24');
+    });
+
+    it('single-digit month and day', () => {
+        expect(formatDate(new Date('2000-01-01T15:12:10')))
+            .toEqual('1/1/2000 15:12');
+    });
+
+    it('puts a 0 for single-digit minutes', () => {
+        expect(formatDate(new Date('2005-12-17T03:05:00')))
+            .toEqual('12/17/2005 3:05');
     });
 });
