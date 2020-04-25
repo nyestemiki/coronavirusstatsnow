1f7286f4be12d1ff18948b07db84f57b5f9f6ea3
Nyeste Miki <nyeste.miki@yahoo.com>
2020-04-25 18:09:22 +0300

    Sample Mocking

---
+++ __test__/mocking.test.js
@@ -0,0 +1,41 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(this.name), 1000);
+        });
+    }
+}
+
+describe('mocking sample', () => {
+    it('mocks a reg function', () => {
+        const sampleFunction = jest.fn();
+
+        sampleFunction('params');
+        expect(sampleFunction).toHaveBeenCalled();
+        expect(sampleFunction).toHaveBeenCalledWith('params');
+        
+        sampleFunction('params');
+        expect(sampleFunction).toHaveBeenCalledTimes(2);
+    });
+
+    it('can create a person', async () => {
+        const name = "name";
+        const p = new Person(name);
+
+        expect(await p.getName()).toBe(name);
+    });
+
+    it('getName mocked function', async () => {
+        const name = "name";
+        const p = new Person(name);
+
+        // Mocking the function: getName()
+        p.getName = jest.fn().mockResolvedValue(name);
+
+        expect(await p.getName()).toBe(name);
+    });
+});
